98f71200c9792a02f722c139357d4108c9f474db
thomas <thomas.carrot38240@gmail.com>
2023-03-20 23:53:22 +0100

    correction du bug, manque 1 animation à faire

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,11 @@
 //
 /* ############ */
+let titreSection = document.querySelector('.titreSection')
+
+window.addEventListener('scroll', ()=>{
+    let value = window.scrollY;
+    let position = -500
+
+    console.log(value)
+})
+
