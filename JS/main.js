a34a6e036fb9468bf43cea22e6445d178a4f938e
thomas <thomas.carrot38240@gmail.com>
2023-03-22 10:31:05 +0100

    ajout animation opacité

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,26 @@ window.addEventListener('scroll', ()=>{
     console.log(value)
 })
 
+const fadeIns = document.querySelectorAll('.carte');
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+};
+
+const callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+        } else {
+            entry.target.style.opacity = 0;
+        }
+    });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+fadeIns.forEach(fadeIn => {
+    observer.observe(fadeIn);
+});
